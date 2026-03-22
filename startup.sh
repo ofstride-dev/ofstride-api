fa1a0044498c227e0fb813489a7dfe639a6efba7
ofstride-dev <ofstride@gmail.com>
2026-03-22 13:12:14 +0530

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+echo "Installing dependencies..."
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+echo "Starting application..."
+python -m uvicorn app:app --host 0.0.0.0 --port ${PORT:-8000}
